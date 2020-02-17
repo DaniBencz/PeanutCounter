ffd5370560f221ab6c5e1b612cc4a67b1fd0331f
Daniel <daniel.bencz@gmail.com>
2020-02-17 09:55:00 +0100

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '/bundle'),
-    filename: 'index_bundle.js'
+    path: path.join(__dirname, '/bundle'), // it has to be absolute path
+    filename: 'index_bundle.js',
+    // publicPath: '' // the path where index.html will look for index_bundle.js
+    // in this case it is same folder as index.html itself, so no need to specify
   },
   devServer: {  // optional
-    historyApiFallback: true
+    historyApiFallback: true  // if a route is not found, it is redirected to the root
     /* inline: true,
     port: 8001 */
   },
-  module: {
+  module: { // a module is an import in a .js file
     rules: [
       {
         test: /\.(js|jsx)$/,  // combination of /\.js$/ /\.jsx?$/
@@ -23,13 +26,18 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [ // allows for several loaders to be used, marked as array
+          // odred is important!, webpack will execute loaders in reverse order
+          'style-loader', // makes .cc available to index.html
+          'css-loader'  // will allow imports in .js files
+        ]
       }
     ]
   },
-  plugins: [
+  plugins: [  // applied on the bundle before it is output
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
-  ]
+    }),
+    /* new webpack.optimize.UglifyJsPlugin({}) */ // removed
+  ],
 }
